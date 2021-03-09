b9f22f35960b589e1041a786d746a6ab51379f7f
Balabanov Denis <dbalabanov@ourgapps.com>
2021-03-09 17:48:43 +0300

    fix review

--- hw01/log_analyzer/config.py
+++ hw01/log_analyzer/config.py
@@ -4,32 +4,31 @@ from typing import Tuple
 
 
 def get_config(cfg) -> Tuple[dict, None]:
-    '''
+    """
     parsing config from args and put values in cfg
     returns None if error
-    '''
+    """
 
     args = parse_args()
-    if args.config:
-        with open(args.config) as json_file:
-            try:
-                data = json.load(json_file)
-                if 'REPORT_SIZE' in data:
-                    cfg['REPORT_SIZE'] = data['REPORT_SIZE']
-                if 'REPORT_DIR' in data:
-                    cfg['REPORT_DIR'] = data['REPORT_DIR']
-                if 'LOG_DIR' in data:
-                    cfg['LOG_DIR'] = data['LOG_DIR']
-                if 'logger' in data:
-                    cfg['logger'] = data['logger']
-            except JSONDecodeError:
-                logging.exception("failed to parse json config")
-                return None
+    if not args.config:
+        return None
+
+    with open(args.config) as json_file:
+        try:
+            data = json.load(json_file)
+            for field in ["REPORT_SIZE", "REPORT_DIR", "LOG_DIR", "logger"]:
+                if field in data:
+                    cfg[field] = data[field]
+        except JSONDecodeError:
+            logging.exception("failed to parse json config")
+            return None
 
     return cfg
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Counts url stats')
-    parser.add_argument('-c', '--config', help='define a json config file')
+    parser = argparse.ArgumentParser(description="Counts url stats")
+    parser.add_argument(
+        "-c", "--config", help="define a json config file", default="./config.json"
+    )
     return parser.parse_args()
--- hw01/log_analyzer/helpers.py
+++ hw01/log_analyzer/helpers.py
@@ -3,55 +3,54 @@ from datetime import datetime
 from collections import namedtuple
 
 rc_log = re.compile(r"nginx-access.*(\d{8})(?:\.gz|\.log)?")
-FileInfo = namedtuple('FileInfo', ['filepath', 'date'])
+FileInfo = namedtuple("FileInfo", ["filepath", "date"])
+
 
 def last_log_from_dir(root_dir: str) -> FileInfo:
-    '''
+    """
     find last log file in root_dir by datetime in file name
-    '''
+    """
     logger = logging.getLogger()
     latest_datetime = datetime.min
-    latest_file = ''
+    latest_file = ""
 
-    for _filename in os.listdir(root_dir):
-        _filepath = os.path.join(root_dir, _filename)
+    for filename in os.listdir(root_dir):
+        filepath = os.path.join(root_dir, filename)
 
-        if os.path.isfile(_filepath):
-            if rc_log.fullmatch(_filename):
-                date_str = rc_log.findall(_filename)[0]
+        if os.path.isfile(filepath):
+            if rc_log.fullmatch(filename):
+                date_str = rc_log.findall(filename)[0]
                 try:
-                    parsed_datetime = datetime.strptime(date_str, '%Y%m%d')
-                    if parsed_datetime > latest_datetime:
-                        latest_datetime = parsed_datetime
-                        latest_file = _filepath
+                    parsed_datetime = datetime.strptime(date_str, "%Y%m%d")
                 except ValueError:
-                    logger.exception(f'failed to parse datetime {date_str}. skipping..')
+                    logger.exception(f"failed to parse datetime {date_str}. skipping..")
+                if parsed_datetime > latest_datetime:
+                    latest_datetime = parsed_datetime
+                    latest_file = filepath
 
     return FileInfo(latest_file, latest_datetime)
 
 
 def gen_report_filename(_dir: str, _date: datetime) -> str:
-    datetime_str = datetime.strftime(_date, '%Y.%m.%d')
-    return os.path.join(_dir, f'report-{datetime_str}.html')
+    datetime_str = datetime.strftime(_date, "%Y.%m.%d")
+    return os.path.join(_dir, f"report-{datetime_str}.html")
 
 
 def log_line_provider(filename: str):
-        '''
-        generator providing line-by-line from log file
-        '''
-        logger = logging.getLogger()
-        logger.info(f'processing file: {filename}')
-        (_, file_encoding) = mimetypes.guess_type(filename)
-        fd = gzip.open(filename, 'rt') if file_encoding == 'gzip' else open(filename, 'r')
+    """
+    generator providing line-by-line from log file
+    """
+    logger = logging.getLogger()
+    logger.info(f"processing file: {filename}")
+    (_, file_encoding) = mimetypes.guess_type(filename)
+    fd = gzip.open(filename, "rt") if file_encoding == "gzip" else open(filename, "r")
 
-        for line in fd:
-            arr = _clean_str(line).split(' ')
-            url = arr[6]
-            req_time = arr[-1]
-            yield (url, req_time)
+    for line in fd:
+        norm_str = re.sub(r"\s{2,}", " ", line)
+        norm_str = re.sub(r'("|\[|\]|\n)', "", norm_str)
+        arr = norm_str.split(" ")
+        url = arr[6]
+        req_time = arr[-1]
+        yield (url, req_time)
 
-        fd.close()
-
-def _clean_str(_str: str) -> str:
-    norm_str = re.sub(r'\s{2,}', ' ', _str)
-    return re.sub(r'("|\[|\]|\n)', '', norm_str)
+    fd.close()
